adaa58665139816dff6a1ce94e446196564a9fad
mikehac <mike.hatskevich@gmail.com>
2023-03-23 18:19:32 +0200

    adding lock on cache object

--- Munters.Assignment/Services/CacheService.cs
+++ Munters.Assignment/Services/CacheService.cs
@@ -19,24 +19,49 @@ namespace Munters.Assignment.Services
             _logger = logger;
         }
 
+        /// <summary>
+        /// This method is build for multiple concurent requests.
+        /// We lock the access to cache object, so only single request 
+        /// can get data from it simultaneously
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
         public async Task<GiphyResponseDTO> GetOrCreate(string cacheKey,Func<Task<GiphyResponseDTO>> func)
         {
+            var semaphore = new SemaphoreSlim(1, 1);
+
             var result = _memoryCache.Get<GiphyResponseDTO>(cacheKey);
             if (result is not null)
             {
                 _logger.LogInformation($"Cache was found for query {cacheKey}");
                 return result;
             }
+            try
+            {
+                await semaphore.WaitAsync();
+                result = _memoryCache.Get<GiphyResponseDTO>(cacheKey);
+                if (result is not null)
+                {
+                    _logger.LogInformation($"Cache was found for query {cacheKey} after recheck");
+                    return result;
+                }
 
-            result = await func();
-            
-            _logger.LogInformation($"The query {cacheKey} was added to the cache");
+                result = await func();
 
-            // Here I limit the time for cache to be saved for 60 seconds
-            // For production I would set it into apsetting configuration file
-            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
-            options.SetSlidingExpiration(TimeSpan.FromSeconds(60));
-            _memoryCache.Set<GiphyResponseDTO>(cacheKey, result, options);
+                _logger.LogInformation($"The query {cacheKey} was added to the cache");
+
+                // Here I limit the time for cache to be saved for 60 seconds
+                // For production I would set it into apsetting configuration file
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+                options.SetSlidingExpiration(TimeSpan.FromSeconds(60));
+                _memoryCache.Set<GiphyResponseDTO>(cacheKey, result, options);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+
             return result;
         }
     }
